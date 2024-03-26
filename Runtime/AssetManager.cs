739146fc271923b71d9a88e83d37683eba1c6cd9
cyhzz <1076357483@qq.com>
2024-03-26 19:25:56 +0800

    ad

---
+++ Runtime/AssetManager.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Com.A9.Singleton;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+namespace Com.A9.AssetManager
+{
+    public class ReadRequest
+    {
+        public string path;
+        public AsyncOperationHandle handle;
+
+        public T GetResult<T>()
+        {
+            return (T)handle.Result;
+        }
+
+        public bool IsDone()
+        {
+            return handle.IsValid() && handle.IsDone;
+        }
+    }
+
+    public class AssetManager : Singleton<AssetManager>
+    {
+        public Dictionary<string, ReadRequest> dic = new Dictionary<string, ReadRequest>();
+
+        public ReadRequest Load<T>(string path, Action OnAllComplete = null)
+        {
+            if (dic.ContainsKey(path))
+            {
+                var req = dic[path];
+                if (req.handle.IsValid() && req.handle.Result != null)
+                {
+                    OnAllComplete?.Invoke();
+                    return req;
+                }
+                else
+                {
+                    StartCoroutine(Load_(new List<string>() { path }));
+                    return req;
+                }
+            }
+            var re = new ReadRequest() { path = path };
+            dic.Add(path, re);
+            StartCoroutine(Load_(new List<string>() { path }));
+            return re;
+        }
+
+        public void Load(List<string> paths, Action OnAllComplete = null)
+        {
+            var path2 = new List<string>();
+            for (int i = 0; i < paths.Count; i++)
+            {
+                if (dic.ContainsKey(paths[i]))
+                {
+                    var req = dic[paths[i]];
+                    if (req.handle.IsValid() && req.handle.Result != null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        path2.Add(paths[i]);
+                    }
+                }
+                else
+                {
+                    path2.Add(paths[i]);
+                }
+            }
+            StartCoroutine(Load_(path2, OnAllComplete));
+        }
+
+        IEnumerator Load_(List<string> paths, Action OnAllComplete = null)
+        {
+            AsyncOperationHandle<IList<IResourceLocation>> locations =
+                   Addressables.LoadResourceLocationsAsync(paths,
+                   Addressables.MergeMode.Union);
+            yield return locations;
+
+            var loads = new List<AsyncOperationHandle>(locations.Result.Count);
+            foreach (var location in locations.Result)
+            {
+                AsyncOperationHandle handle = new AsyncOperationHandle();
+                if (AssetType.GetAssetType(location.PrimaryKey) == typeof(GameObject))
+                    handle = Addressables.LoadAssetAsync<GameObject>(location);
+                else if (AssetType.GetAssetType(location.PrimaryKey) == typeof(Sprite))
+                    handle = Addressables.LoadAssetAsync<Sprite>(location.PrimaryKey);
+
+                // handle.Completed+=obj=>
+                handle.Completed += obj =>
+                {
+                    if (dic.ContainsKey(location.PrimaryKey))
+                    {
+                        dic[location.PrimaryKey].handle = handle;
+                    }
+                    else
+                    {
+                        var nw_req = new ReadRequest()
+                        {
+                            path = location.PrimaryKey,
+                            handle = handle
+                        };
+                        dic.Add(location.PrimaryKey, nw_req);
+                    }
+                };
+                loads.Add(handle);
+            }
+            yield return Addressables.ResourceManager.CreateGenericGroupOperation(loads, true);
+            OnAllComplete?.Invoke();
+        }
+
+        public T Get<T>(string str) where T : class
+        {
+            foreach (var item in dic)
+            {
+                var req = item.Value;
+                if (req.handle.Result != null && ((UnityEngine.Object)req.handle.Result).name == str)
+                {
+                    return (T)req.handle.Result;
+                }
+            }
+            return null;
+        }
+
+        public void Unload(string path)
+        {
+            if (dic.ContainsKey(path) == false)
+            {
+                return;
+            }
+            Addressables.Release(dic[path].handle);
+        }
+    }
+
+}
+
---
+++ Runtime/AssetType.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Com.A9.AssetManager
+{
+    public static class AssetType
+    {
+        public static Type GetAssetType(string assetName)
+        {
+            if (AssetType.IsPrefabFile(assetName))
+            {
+                return typeof(GameObject);
+            }
+            else if (AssetType.IsTextureFile(assetName))
+            {
+                return typeof(Sprite);
+            }
+            else if (AssetType.IsAudioFile(assetName))
+            {
+                return typeof(AudioClip);
+            }
+            else if (AssetType.IsTextFile(assetName))
+            {
+                return typeof(TextAsset);
+            }
+            return typeof(UnityEngine.Object);
+        }
+
+        public static bool IsPrefabFile(string path)
+        {
+            return path.EndsWith(".prefab", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsTextFile(string path)
+        {
+            return path.EndsWith(".bytes", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".json", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".txt", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsAudioFile(string path)
+        {
+            return path.EndsWith(".ogg", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".wav", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsTextureFile(string path)
+        {
+            return path.EndsWith(".png", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase)
+                   || path.EndsWith(".tga", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsShaderFile(string path)
+        {
+            return path.EndsWith(".shader", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsAnimFile(string path)
+        {
+            return path.EndsWith(".anim", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
